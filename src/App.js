216b8ac5f3ce56c545ee7710ad5eb53dcf6dd8d3
badai <winata.afa@gmail.com>
2019-05-28 19:45:07 +0700

    initial boiler plate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Firstname from './components/Firstname';
+import Lastname from './components/Lastname';
+import Gender from './components/Gender';
+import Address from './components/Address';
+import Birthday from './components/Birthday';
+import Hobby from './components/Hobby';
+import Interest from './components/Interest';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Firstname/>
+      <Lastname/>
+      <Gender/>
+      <Address/>
+      <Birthday/>
+      <Hobby/>
+      <Interest/>
     </div>
   );
 }
---
+++ src/components/Birthday.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Birthday extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
